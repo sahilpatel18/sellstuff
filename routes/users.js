8a2b72adbbaf1012d1cb0f0a2cf7740523a03707
sahilpatel18 <sahil.41801@gmail.com>
2020-06-17 15:23:03 -0400

    view all products page

--- routes/users.js
+++ routes/users.js
@@ -11,49 +11,66 @@ router.get("/add-product", (req, res) => {
   });
 });
 
-router.post("/add-product", async (req, res) => {
-  let title = req.body.title;
-  let description = req.body.description;
-  let price = parseFloat(req.body.price);
-  let userId = req.session.user.userId;
+router.get('/products',async (req,res) => {
 
-  let product = models.Product.build({
-    title: title,
-    description: description,
-    price: price,
-    userId: userId,
-    imageURL: uniqueFilename,
-  });
-
-  let persistedProduct = await product.save();
-  if (persistedProduct != null) {
-    res.redirect("/user/products");
-  } else {
-    res.render("users/add-product", { message: "Unable to add product" });
-  }
-});
-
-function uploadFile(req, callback) {
-  new formidable.IncomingForm()
-    .parse(req)
-    .on("fileBegin", (name, file) => {
-      uniqueFilename = `${uuidv4()}.${file.name.split(".").pop()}`;
-      file.name = uniqueFilename;
-      file.path = __basedir + "/uploads/" + file.name;
+    let products = await models.Product.findAll({
+      where: {
+        userId: req.session.user.userId
+      }
     })
-    .on("file", (name, file) => {
-      callback(file.name);
-    });
-}
-
-router.post("/upload", (req, res) => {
-  uploadFile(req, (photoURL) => {
-    photoURL = `/uploads/${photoURL}`;
-    res.render("users/add-product", {
-      imageURL: photoURL,
-      className: "product-preview-image",
-    });
-  });
-});
-
-module.exports = router;
+  
+    res.render('users/products',{products: products})
+  
+  })
+  
+  router.post('/add-product',async (req,res) => {
+  
+    let title = req.body.title
+    let description = req.body.description
+    let price = parseFloat(req.body.price)
+    let userId = req.session.user.userId
+  
+    let product = models.Product.build({
+      title: title,
+      description: description,
+      price: price,
+      userId: userId,
+      imageURL: uniqueFilename
+    })
+  
+    let persistedProduct = await product.save()
+    if(persistedProduct != null) {
+      res.redirect('/users/products')
+    } else {
+      res.render('users/add-product',{message: 'Unable to add product'})
+    }
+  
+  })
+  
+  function uploadFile(req,callback) {
+  
+    new formidable.IncomingForm().parse(req)
+    .on('fileBegin',(name,file) => {
+  
+        uniqueFilename = `${uuidv4()}.${file.name.split('.').pop()}`
+        file.name = uniqueFilename
+        file.path = __basedir + '/uploads/' + file.name
+    })
+    .on('file',(name,file) => {
+      callback(file.name)
+    })
+  
+  }
+  
+  router.post('/upload',(req,res) => {
+  
+    uploadFile(req,(photoURL) => {
+      photoURL = `/uploads/${photoURL}`
+      res.render('users/add-product',{imageURL: photoURL, className: 'product-preview-image'})
+    })
+  
+  })
+  
+  
+  module.exports = router
+  
