3f15723f2b081da178bbaa48a728222f5d132bc0
sahilpatel18 <sahil.41801@gmail.com>
2020-06-17 14:57:16 -0400

    photo upload and formidable setup

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
-const express = require("express");
-const app = express();
-const mustacheExpress = require("mustache-express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const models = require("./models");
-const bcrypt = require("bcrypt");
-const session = require("express-session");
-const indexRoutes = require("./routes/index");
-const userRoutes = require("./routes/users");
 
-const PORT = 3000;
-const VIEWS_PATH = path.join(__dirname, "/views");
+const express = require('express')
+const app = express()
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const models = require('./models')
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+const indexRoutes = require('./routes/index')
+const userRoutes = require('./routes/users')
 
+const PORT = 3000
+const VIEWS_PATH = path.join(__dirname,'/views')
 
-global.__basedir = __dirname
+global.__basedir = __dirname 
 
-app.use(
-  session({
-    secret: "somesecret",
-    resave: true,
-    saveUninitialized: false,
-  })
-);
+app.use(session({
+  secret: 'somesecret',
+  resave: true,
+  saveUninitialized: false
+}))
 
+// static folder
+app.use('/uploads',express.static('uploads'))
+app.use('/css',express.static('css'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/uploads', express.static('uploads'))
+app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials','.mustache'))
+app.set('views',VIEWS_PATH)
+app.set('view engine','mustache')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/',indexRoutes)
+app.use('/users',userRoutes)
 
-app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
-app.set("views", VIEWS_PATH);
-app.set("view engine", "mustache");
-
-app.use("/", indexRoutes);
-app.use("/users", userRoutes);
-app.listen(PORT, () => {
-  console.log("server is running");
-});
+app.listen(PORT,() => console.log('Server is running...'))
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,59 @@
 const express = require("express");
 const router = express.Router();
-const formidable = require('formidable')
+const formidable = require("formidable");
+const { v4: uuidv4 } = require("uuid");
+const models = require("../models");
+let uniqueFilename = "";
+
 router.get("/add-product", (req, res) => {
-  res.render("users/add-product");
+  res.render("users/add-product", {
+    className: "product-preview-image-invisible",
+  });
 });
 
-function uploadFile(req,callback){
-     new formidable.IncomingForm().parse(req)
-     .on('fileBegin', (name,file) => {
-         file.path = __basedir + '/uploads/' + file.name
-     })
-     .on('file',(name,file) => {
-        callback(file.name)
-     })
+router.post("/add-product", async (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let price = parseFloat(req.body.price);
+  let userId = req.session.user.userId;
+
+  let product = models.Product.build({
+    title: title,
+    description: description,
+    price: price,
+    userId: userId,
+    imageURL: uniqueFilename,
+  });
+
+  let persistedProduct = await product.save();
+  if (persistedProduct != null) {
+    res.redirect("/user/products");
+  } else {
+    res.render("users/add-product", { message: "Unable to add product" });
+  }
+});
+
+function uploadFile(req, callback) {
+  new formidable.IncomingForm()
+    .parse(req)
+    .on("fileBegin", (name, file) => {
+      uniqueFilename = `${uuidv4()}.${file.name.split(".").pop()}`;
+      file.name = uniqueFilename;
+      file.path = __basedir + "/uploads/" + file.name;
+    })
+    .on("file", (name, file) => {
+      callback(file.name);
+    });
 }
 
-router.post('/upload', (req,res) => {
-
-    uploadFile(req,(photoURL) => {
-        res.send('UPLOAD')
-    })
-
-})
+router.post("/upload", (req, res) => {
+  uploadFile(req, (photoURL) => {
+    photoURL = `/uploads/${photoURL}`;
+    res.render("users/add-product", {
+      imageURL: photoURL,
+      className: "product-preview-image",
+    });
+  });
+});
 
 module.exports = router;
