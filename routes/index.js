8ffe836b638498668934870c1d5e89e578243027
sahilpatel18 <sahil.41801@gmail.com>
2020-06-18 12:15:32 -0400

    product edit page and details

--- routes/index.js
+++ routes/index.js
@@ -1,93 +1,88 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const models = require('../models')
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const models = require("../models");
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
-router.get('/login',(req,res) => {
-  res.render('login')
+router.get('/products/:productId', async (req,res) => {
+  const productId = req.params.productId
+  const product = models.Product.findByPk(productId)
+  res.render('product-details', product.dataValues )
 })
 
-router.get('/register',(req,res) => {
-  res.render('register')
+router.get('/', async (req,res) => {
+  let products = await models.Product.findAll()
+  res.render('index',{products: products})
 })
 
-router.post('/register',async (req,res) => {
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
-  let username = req.body.username
-  let password = req.body.password
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
   let persistedUser = await models.User.findOne({
     where: {
-      username: username
-    }
-  })
+      username: username,
+    },
+  });
 
-  if(persistedUser == null) {
-
+  if (persistedUser == null) {
     bcrypt.hash(password, SALT_ROUNDS, async (error, hash) => {
-
-      if(error) {
-        res.render('/register',{message: 'Error creating user!'})
+      if (error) {
+        res.render("/register", { message: "Error creating user!" });
       } else {
-
         let user = models.User.build({
           username: username,
-          password: hash
-        })
+          password: hash,
+        });
 
-        let savedUser = await user.save()
-        if(savedUser != null) {
-          res.redirect('/login')
+        let savedUser = await user.save();
+        if (savedUser != null) {
+          res.redirect("/login");
         } else {
-          res.render('/register',{message: "User already exists!"})
+          res.render("/register", { message: "User already exists!" });
         }
-
       }
-
-    })
-
+    });
   } else {
-      res.render('/register',{message: "User already exists!"})
+    res.render("/register", { message: "User already exists!" });
   }
+});
 
-})
-
-router.post('/login', async (req,res) => {
-
-  let username = req.body.username
-  let password = req.body.password
+router.post("/login", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
   let user = await models.User.findOne({
     where: {
-      username: username
-    }
-  })
+      username: username,
+    },
+  });
 
-  if(user != null) {
-
-    bcrypt.compare(password, user.password,(error, result) => {
-
-      if(result) {
-
+  if (user != null) {
+    bcrypt.compare(password, user.password, (error, result) => {
+      if (result) {
         // create a session
-        if(req.session) {
-          req.session.user = {userId: user.id}
-          res.redirect('/users/products')
+        if (req.session) {
+          req.session.user = { userId: user.id };
+          res.redirect("/users/products");
         }
-
       } else {
-        res.render('login',{message: 'Incorrect username or password'})
+        res.render("login", { message: "Incorrect username or password" });
       }
-
-    })
-
-  } else { // if the user is null
-    res.render('login',{message: 'Incorrect username or password'})
+    });
+  } else {
+    // if the user is null
+    res.render("login", { message: "Incorrect username or password" });
   }
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -11,66 +11,110 @@ router.get("/add-product", (req, res) => {
   });
 });
 
-router.get('/products',async (req,res) => {
+router.post('/update-product', async (req,res) => {
+    const productId = req.body.productId
+    const title = req.body.title
+    const description = req.body.description
+    const price = parseFloat(req.body.price)
 
-    let products = await models.Product.findAll({
-      where: {
-        userId: req.session.user.userId
-      }
+    const result = await models.Product.update({
+        title:title,
+        description: description,
+        price:price,
+        imageURL: uniqueFilename
+    },{
+        where: {
+            id: productId
+        }
     })
+
+    res.redirect('/users/products')
+})
+
+
+router.post('/upload/edit/:productId',(req,res) => {
+
+    uploadFile(req, async (photoURL) => {
   
-    res.render('users/products',{products: products})
+      let productId = parseInt(req.params.productId)
+      let product = await models.Product.findByPk(productId)
+  
+      let response = product.dataValues
+      response.imageURL = photoURL
+  
+      res.render('users/edit',response)
+    })
   
   })
-  
-  router.post('/add-product',async (req,res) => {
-  
-    let title = req.body.title
-    let description = req.body.description
-    let price = parseFloat(req.body.price)
-    let userId = req.session.user.userId
-  
-    let product = models.Product.build({
-      title: title,
-      description: description,
-      price: price,
-      userId: userId,
-      imageURL: uniqueFilename
-    })
-  
-    let persistedProduct = await product.save()
-    if(persistedProduct != null) {
-      res.redirect('/users/products')
-    } else {
-      res.render('users/add-product',{message: 'Unable to add product'})
-    }
-  
-  })
-  
-  function uploadFile(req,callback) {
-  
-    new formidable.IncomingForm().parse(req)
-    .on('fileBegin',(name,file) => {
-  
-        uniqueFilename = `${uuidv4()}.${file.name.split('.').pop()}`
-        file.name = uniqueFilename
-        file.path = __basedir + '/uploads/' + file.name
-    })
-    .on('file',(name,file) => {
-      callback(file.name)
-    })
-  
+router.get("/products/:productId", async (req, res) => {
+  let productId = req.params.productId;
+  let product = await models.Product.findByPk(productId);
+  res.render("users/edit", product.dataValues);
+});
+
+router.post("/delete-product", async (req, res) => {
+  let productId = parseInt(req.body.productId);
+  let result = await models.Product.destroy({
+    where: {
+      id: productId,
+    },
+  });
+  res.redirect("/users/products");
+});
+
+router.get("/products", async (req, res) => {
+  let products = await models.Product.findAll({
+    where: {
+      userId: req.session.user.userId,
+    },
+  });
+
+  res.render("users/products", { products: products });
+});
+
+router.post("/add-product", async (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let price = parseFloat(req.body.price);
+  let userId = req.session.user.userId;
+
+  let product = models.Product.build({
+    title: title,
+    description: description,
+    price: price,
+    userId: userId,
+    imageURL: uniqueFilename,
+  });
+
+  let persistedProduct = await product.save();
+  if (persistedProduct != null) {
+    res.redirect("/users/products");
+  } else {
+    res.render("users/add-product", { message: "Unable to add product" });
   }
-  
-  router.post('/upload',(req,res) => {
-  
-    uploadFile(req,(photoURL) => {
-      photoURL = `/uploads/${photoURL}`
-      res.render('users/add-product',{imageURL: photoURL, className: 'product-preview-image'})
+});
+
+function uploadFile(req, callback) {
+  new formidable.IncomingForm()
+    .parse(req)
+    .on("fileBegin", (name, file) => {
+      uniqueFilename = `${uuidv4()}.${file.name.split(".").pop()}`;
+      file.name = uniqueFilename;
+      file.path = __basedir + "/uploads/" + file.name;
     })
-  
-  })
-  
-  
-  module.exports = router
-  
+    .on("file", (name, file) => {
+      callback(file.name);
+    });
+}
+
+router.post("/upload", (req, res) => {
+  uploadFile(req, (photoURL) => {
+    photoURL = `/uploads/${photoURL}`;
+    res.render("users/add-product", {
+      imageURL: photoURL,
+      className: "product-preview-image",
+    });
+  });
+});
+
+module.exports = router;
